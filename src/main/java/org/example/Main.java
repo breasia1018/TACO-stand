1875d80381f9fbadfa86bae2327ceaef7b68add6
breasiabarringer <breasia1018@gmail.com>
2026-05-28 14:51:29 -0400

    Done Now :)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,35 @@
 package org.example;
+
 public class Main {
+
+    public static final String RESET = "\u001B[0m";
+    public static final String YELLOW = "\u001B[33;1m";
 
     public static void main(String[] args) {
 
+        printBanner();
+
         TacoStoreApp app = new TacoStoreApp();
         app.start();
+    }
+
+    public static void printBanner() {
+
+        System.out.println(YELLOW + "========================================" + RESET);
+        System.out.println();
+
+        System.out.println(YELLOW + "███████╗ ████╗  █████╗ █████╗" + RESET);
+        System.out.println(YELLOW + "╚══█══╝██╔══██╗██╔════╝██╔═══██╗" + RESET);
+        System.out.println(YELLOW + "   █║   █████║██║     ██║   ██║" + RESET);
+        System.out.println(YELLOW + "   █║   ██╔══█║██║     ██║   ██║" + RESET);
+        System.out.println(YELLOW + "   █║   ██║  █║╚██████╗╚██████╔╝" + RESET);
+        System.out.println(YELLOW + "   ╚═╝   ╚═╝  ╚═╝ ╚═════╝ ╚═════╝" + RESET);
+
+        System.out.println();
+        System.out.println(YELLOW + "        🌮 T A C O   S T O R E 🌮" + RESET);
+        System.out.println(YELLOW + "     Fresh • Fast • Flavorful" + RESET);
+
+        System.out.println();
+        System.out.println(YELLOW + "========================================" + RESET);
     }
 }
